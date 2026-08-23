f655054a61b4ebcab5de03e937019609240f02f5
Naheun Alam <naheunpranto258@gmail.com>
2026-08-23 16:04:41 +0600

    add destination form and create post api

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,28 +4,30 @@ import React from 'react';
 
 const Navbar = () => {
     return (
-        <nav className='flex justify-between p-5 bg-gray-100'>
-            <ul className='flex gap-3'>
-                <li><Link href={"/"}>Home</Link></li>
-                <li><Link href={"/destination"}>Destination</Link></li>
-                <li><Link href={"/myBookings"}>My Bookings</Link></li>
-                <li><Link href={"/admin"}>Admin</Link></li>
-            </ul>
+        <nav className='bg-gray-100 p-5'>
+            <div className='flex justify-between container mx-auto'>
+                <ul className='flex gap-3'>
+                    <li><Link href={"/"}>Home</Link></li>
+                    <li><Link href={"/destination"}>Destination</Link></li>
+                    <li><Link href={"/myBookings"}>My Bookings</Link></li>
+                    <li><Link href={"/addDestination"}>Add Destination</Link></li>
+                </ul>
 
-            <div>
-                <Image
-                    src={"/assets/Wanderlast.png"}
-                    height={100}
-                    width={100}
-                    alt=''
-                />
+                <div>
+                    <Image
+                        src={"/assets/Wanderlast.png"}
+                        height={100}
+                        width={100}
+                        alt=''
+                    />
+                </div>
+
+                <ul className='flex gap-3'>
+                    <li><Link href={"/profile"}>Profile</Link></li>
+                    <li><Link href={"/login"}>Login</Link></li>
+                    <li><Link href={"/signup"}>SingUp</Link></li>
+                </ul>
             </div>
-
-            <ul className='flex gap-3'>
-                <li><Link href={"/profile"}>Profile</Link></li>
-                <li><Link href={"/login"}>Login</Link></li>
-                <li><Link href={"/signup"}>SingUp</Link></li>
-            </ul>
         </nav>
     );
 };
