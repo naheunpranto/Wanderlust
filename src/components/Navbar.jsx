abe201db8accc2c1fda7a1e6535952c33301b168
Naheun Alam <naheunpranto258@gmail.com>
2026-08-24 12:42:39 +0600

    implementing login page

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,35 +1,78 @@
-import Image from 'next/image';
-import Link from 'next/link';
-import React from 'react';
+"use client";
+
+import { authClient } from "@/lib/auth-client";
+import { Avatar, Button } from "@heroui/react";
+import Image from "next/image";
+import Link from "next/link";
+import React from "react";
 
 const Navbar = () => {
-    return (
-        <nav className='bg-gray-100 p-5'>
-            <div className='flex justify-between container mx-auto'>
-                <ul className='flex gap-3'>
-                    <li><Link href={"/"}>Home</Link></li>
-                    <li><Link href={"/destination"}>Destination</Link></li>
-                    <li><Link href={"/myBookings"}>My Bookings</Link></li>
-                    <li><Link href={"/addDestination"}>Add Destination</Link></li>
-                </ul>
+  const { data: session } = authClient.useSession();
 
-                <div>
-                    <Image
-                        src={"/assets/Wanderlast.png"}
-                        height={100}
-                        width={100}
-                        alt=''
-                    />
-                </div>
+  const user = session?.user;
+  console.log(user);
 
-                <ul className='flex gap-3'>
-                    <li><Link href={"/profile"}>Profile</Link></li>
-                    <li><Link href={"/login"}>Login</Link></li>
-                    <li><Link href={"/signup"}>SingUp</Link></li>
-                </ul>
-            </div>
-        </nav>
-    );
+  return (
+    <nav className="bg-gray-100 p-5">
+      <div className="flex justify-between container mx-auto items-center">
+        <ul className="flex gap-3">
+          <li>
+            <Link href={"/"}>Home</Link>
+          </li>
+          <li>
+            <Link href={"/destination"}>Destination</Link>
+          </li>
+          <li>
+            <Link href={"/myBookings"}>My Bookings</Link>
+          </li>
+          <li>
+            <Link href={"/addDestination"}>Add Destination</Link>
+          </li>
+        </ul>
+
+        <div>
+          <Image
+            src={"/assets/Wanderlast.png"}
+            height={100}
+            width={100}
+            alt=""
+          />
+        </div>
+
+        <ul className="flex items-center gap-3">
+          <li>
+            <Link href={"/profile"}>Profile</Link>
+          </li>
+
+          {user ? (
+            <>
+              <li>
+                <Avatar>
+                  <Avatar.Image
+                    alt="John Doe"
+                    src={user?.image}
+                  />
+                  <Avatar.Fallback>{user.name[0]}</Avatar.Fallback>
+                </Avatar>
+              </li>
+              <li>
+                <Button variant="danger" className={"rounded-none"}>Logout</Button>
+              </li>
+            </>
+          ) : (
+            <>
+              <li>
+                <Link href={"/login"}>Login</Link>
+              </li>
+              <li>
+                <Link href={"/signup"}>SingUp</Link>
+              </li>
+            </>
+          )}
+        </ul>
+      </div>
+    </nav>
+  );
 };
 
 export default Navbar;
